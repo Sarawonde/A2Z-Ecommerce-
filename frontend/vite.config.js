704f064463a20f7db4428baf50fc0113eaab13c2
sara <saronwondwossen78ggmail.com>
2026-08-13 21:34:38 +0300

    Fix vite.config.js syntax errors

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,8 +4,8 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    react(), tailwindcss()],
+  plugins: [react(), tailwindcss()],
+  base: process.env.VITE_BASE_PATH || "/A2Z-Ecommerce-",
   server: {
     proxy: {
       '/api': 'http://localhost:4000',
